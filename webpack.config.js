a2a5445e1f9ec8d681eef16773379fa4acb81ff2
chenyiliang <chenyiliangconan@qq.com>
2017-05-03 15:48:40 +0800

    react 15 webpack 2 简易开发配置

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/,
+            include: path.join(__dirname, 'app')
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        historyApiFallback: true,
+        hot: true
+    }
+}
