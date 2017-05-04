37a87448c348fb6d2c3f794191fa8958d675a73d
chenyiliang <chenyiliangconan@qq.com>
2017-05-04 10:23:20 +0800

    react event

--- app/index.js
+++ app/index.js
@@ -10,4 +10,80 @@ class App extends Component {
     }
 }
 
-ReactDOM.render(<App name="World"/>, document.getElementById('container'));
+class Hello extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('init state');
+        this.state = {
+            opacity: 1.0,
+            fontSize: '50px'
+        };
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        setTimeout(()=> {
+            console.log(this.state);
+            this.setState({
+                opacity: 1.0,
+                fontSize: '100px'
+            });
+            console.log(this.state);
+        }, 2000);
+    }
+
+    render() {
+        console.log('render');
+        return (<div style={this.state}>Life Cycle</div>);
+    }
+}
+
+class ClickComponent extends Component {
+
+    render() {
+        return (<div>
+            <button onClick={this.handleClick.bind(this)}>显示|隐藏</button>
+            <span ref={span=>this.tipSpan=span}>测试点击</span></div>);
+    }
+
+    handleClick(event) {
+        if (this.tipSpan.style.display === 'none') {
+            this.tipSpan.style.display = 'inline';
+        } else {
+            this.tipSpan.style.display = 'none';
+        }
+        event.stopPropagation();
+        event.preventDefault();
+    }
+}
+
+class InputComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputContent: ''
+        };
+    }
+
+    changeHandler(event) {
+        this.setState({
+            inputContent: event.target.value
+        });
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    render() {
+        return (<div><input type="text" onChange={this.changeHandler.bind(this)}/><span>{this.state.inputContent}</span>
+        </div>);
+    }
+}
+
+ReactDOM.render(<div><App/><Hello/><ClickComponent/><InputComponent/></div>, document.getElementById('container'));
+
+
